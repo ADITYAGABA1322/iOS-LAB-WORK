decb88f1809aa41df3e85437c60181284b7e9b5b
Xcode_Addy <97380595+ADITYAGABA1322@users.noreply.github.com>
2024-04-28 15:53:27 +0530

    add

---
+++ Guided Projects/RainbowTabsClass/RainbowTabsClass/RedViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "1"
+    }
+
+
+}
+
+  
